85a174c8ff9ef64d94d462174658e7c84a916615
damccull <401104+damccull@users.noreply.github.com>
2024-07-10 10:00:13 -0500

    Fix nix command

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-nix --extra-experimental-features "nix-commands flakes" github:damccull/kickstart-nix.nvim
+nix --extra-experimental-features "nix-command flakes" profile install github:damccull/kickstart-nix.nvim
 
